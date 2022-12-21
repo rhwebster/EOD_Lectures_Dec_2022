2da92722879511a5461c024fa09d7174f76ed095
Ryan Webster <ryan.h.webster@gmail.com>
2022-12-20 16:35:53 -0800

    remove solutions

--- Wk1/day2/myIndexOf.js
+++ Wk1/day2/myIndexOf.js
@@ -1,18 +1,3 @@
 function myIndexOf(input, target) {
-   let i = 0;
-
-   while (input[i] !== undefined) {
-    if (input[i] === target) {
-        return i;
-    }
-    i++;
-   }
-
-   return -1;
+  
 };
-
-
-console.log(myIndexOf('bootcamp', 'o'));
-console.log('bootcamp'.indexOf('o'));
-console.log(myIndexOf([1, 2, 3, 4, 5], 5));
-console.log([1, 2, 3, 4, 5].indexOf(5));
