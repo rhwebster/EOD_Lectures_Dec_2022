8e31e7779dfc288b7b37c0532a081737b1901def
Ryan Webster <ryan.h.webster@gmail.com>
2023-01-04 16:06:42 -0800

    array methods

---
+++ Wk2/day3/hipsterfy.js
@@ -0,0 +1,26 @@
+/*
+Write a function `hipsterfy(sentence)` that takes in a sentence string and
+returns the sentence where every word is missing it's last vowel.
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+Examples:
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
+*/
+
+let removeLastVowel = function (word) {
+    
+};
+
+let hipsterfy = function (sentence) {
+    
+};
+
+console.log(removeLastVowel('hello'));
+console.log(removeLastVowel('goodbye'));
+console.log(removeLastVowel('please'));
+console.log(removeLastVowel('rhythm'));
+console.log(hipsterfy('When should everyone wake up?')); // 'Whn shold everyon wak p?'
+console.log(hipsterfy('get ready for our bootcamp')); // 'gt redy fr or bootcmp'
+console.log(hipsterfy('panthers are great animals')); // 'panthrs ar gret animls'
