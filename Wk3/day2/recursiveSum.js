4355796dcde9eae593b4a003d9e84d039de007f0
Ryan Webster <ryan.h.webster@gmail.com>
2023-01-10 14:01:21 -0800

    tuesday

---
+++ Wk3/day2/recursiveSum.js
@@ -0,0 +1,15 @@
+//Recursive Sum
+
+/* Write a function called recursive sum, that takes in an array consisting of 
+numbers and nested arrays consisting of numbers (and nested nested arrays, and so
+on). The function return the sum of all of the numbers within the array, regardless 
+of nesting. 
+Examples:
+recursiveSum([1, 2, 3, 4, 5, 6, 7]) => 28
+recursiveSum([1, 2, [3, 4], [5, 6, 7]]) => 28
+recursiveSum([1, 2, [3, 4], [5, [6, 7]]]) => 28
+*/
+
+function recursiveSum(arr) {
+
+};
