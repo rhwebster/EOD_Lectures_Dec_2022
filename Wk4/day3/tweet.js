8f082d47e65bd98ddbdfeb244d074153296d581f
Ryan Webster <ryan.h.webster@gmail.com>
2023-01-18 13:40:09 -0800

    tweet and user

---
+++ Wk4/day3/tweet.js
@@ -0,0 +1,6 @@
+/* Create a class 'Tweet' that has a text body and a method 'printTweet' that
+ prints the body of the tweet */
+
+
+
+/* export the tweet class */
---
+++ Wk4/day3/user.js
@@ -0,0 +1,42 @@
+/* 
+1. Create a class User that has the following attributes:
+    a. name
+    b. username
+    c. email
+    d. password
+    e. bio
+    f. follows - starts out as an empty array
+    g. followers - starts out as an empty array
+    h. tweets - starts out as an empty array
+*/
+
+
+
+/* 
+Add the following methods:
+    1. getUsername: which returns the user's username
+    2. userInfo: which prints the user's name, username, email, and bio
+    3. changeUsername: which changes the username to the newly assigned username,
+    and prints 'Your new username is <new username>'
+*/
+
+
+
+/* 
+4. changePassword: which changes the password and let's you know it's been 
+changed with the message 'Your password has been updated'
+5. follow: adds a user to the user's following array
+6. unfollow: removes a user from the user's following array
+*/
+
+
+
+/*
+7. showFollows: shows a list of the usernames of the users they follow
+8. addTweet: which adds a new tweet to the user's list of tweets
+9: allTweets: that prints the contents of all of the user's tweets
+*/
+
+
+
+/* export the user class */
