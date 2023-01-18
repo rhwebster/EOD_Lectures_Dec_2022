b8f04bc612aeab2e765cdea5d4b64499640b71d3
Ryan Webster <ryan.h.webster@gmail.com>
2023-01-18 13:36:05 -0800

    twitter

---
+++ Wk4/day3/basicTwitter.js
@@ -0,0 +1,34 @@
+/* Import the tweet and user classes */
+
+
+
+const ryan = new User("Ryan", "rwester", "rw@aa.io", "12345", "July '22 Cohort Lead");
+const shane = new User("Shane", "swilkey");
+const briana = new User("Briana", "brobinson");
+const gary = new User("Gary", "gsong");
+const charlie = new User("Charlie", "csheppard");
+
+let tweet1 = new Tweet("Welcome to Mod 2 and OOP!!!");
+let tweet2 = new Tweet("This is where things start to get fun!")
+
+/* Check user info and fix any typos */
+
+
+
+/* Follow Shane and Briana */
+
+
+
+/* Follow Gary and Charlie, and unfollow Shane */
+
+
+
+/* Show who you're following */
+
+
+
+/* Add tweet1 and print it */
+
+
+
+/* Add tweet2 and print all tweets */
