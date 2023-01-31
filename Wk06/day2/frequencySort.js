2cd09bb8221f4b4cb09cd487f3fa1d9e53aa4ede
Ryan Webster <ryan.h.webster@gmail.com>
2023-01-31 14:42:53 -0800

    odd even sort

---
+++ Wk06/day2/frequencySort.js
@@ -0,0 +1,14 @@
+/*
+Given an array of integers, sort the array in increasing order based on the frequency 
+of the values. If multiple values have the same frequency, sort them in decreasing 
+order. Return the sorted array.
+
+Example:
+    const arr1 = [1, 1, 2, 2, 2, 3];
+    const arr2 = [2, 3, 1, 3, 2];
+    const arr3 = [-1, 1, -6, 4, 5, -6, 1, 4, 1];
+
+    frequencySort(arr1); // => [3, 1, 1, 2, 2, 2];
+    frequencySort(arr2); // => [1, 3, 3, 2, 2];
+    frequencySort(arr3); // => [5, -1, 4, 4, -6, -6, 1, 1, 1];
+*/
---
+++ Wk06/day2/validAnagrams.js
@@ -0,0 +1,10 @@
+/*
+Given two strings, s and t, write a function that determines if s and t are anagrams
+
+Example:
+    let s = "anagram";
+    let t = "nagaram";
+
+    validAnagrams(s, t); => true
+*/
+
