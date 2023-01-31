615d163e7526602dafb9582856f420350d1fb163
Ryan Webster <ryan.h.webster@gmail.com>
2023-01-31 14:46:13 -0800

    wednesday problems

---
+++ Wk06/day2/oddEvenSort.js
@@ -0,0 +1,11 @@
+/*
+Given an array, sort it such that the odds appear in ascending order on the left, 
+then the evens on the right. (Remember, try to do this in-place!).
+
+Example:
+    const arr1 = [5, 4, 7, 2, 9, 8, 1, 6, 3];
+    const arr2 = [5, 8, 13, 6, 22, 14, 9];
+
+    oddEvenSort(arr1); // => [1, 3, 5, 7, 9, 2, 4, 6, 8]
+    oddEvenSort(arr2); // => [5, 9, 13, 6, 8, 14, 22]
+*/
