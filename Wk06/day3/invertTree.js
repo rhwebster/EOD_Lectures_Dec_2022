43b21311d85bfa175e0fe3646d82e08015e52d97
Ryan Webster <ryan.h.webster@gmail.com>
2023-02-01 15:56:12 -0800

    wednesday

---
+++ Wk06/day3/invertTree.js
@@ -0,0 +1,18 @@
+/*
+Given the root of a binary tree, invert the tree, and return its root.
+
+Input:   tree = 1               
+               / \                 
+              2   3               
+             /   / \             
+            5   6   7           
+Output:  tree = 1
+               / \
+              3   2
+             / \   \
+            7   6   5
+*/
+
+const invertTree = function(root) {
+
+}
---
+++ Wk06/day3/isSameTree.js
@@ -0,0 +1,18 @@
+/*
+Given the roots of two binary trees p and q, write a function to check if they are 
+the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and 
+the nodes have the same value.
+
+Input:     p =  1               q = 1
+               / \                 / \
+              2   3               2   3
+             /   / \             /   / \
+            5   6   7           5   6   7
+Output: true
+*/
+
+const isSameTree = function (p, q) {
+    
+};
