91d5ebffb28af3e541eb86d33814aa249f419023
Ryan Webster <ryan.h.webster@gmail.com>
2023-02-01 16:58:17 -0800

    wednesday solutions

--- Wk06/day3/invertTree.js
+++ Wk06/day3/invertTree.js
@@ -2,9 +2,9 @@
 Given the root of a binary tree, invert the tree, and return its root.
 
 Input:   tree = 1               
-               / \                 
+               R L               
               2   3               
-             /   / \             
+             R   R L             
             5   6   7           
 Output:  tree = 1
                / \
@@ -14,5 +14,13 @@ Output:  tree = 1
 */
 
 const invertTree = function(root) {
+    if (!root) return null;
 
-}
+    invertTree(root.right);
+    invertTree(root.left);
+
+    let curr = root.left;
+    root.left = root.right;
+    root.right = curr;
+    return root;
+};
--- Wk06/day3/isSameTree.js
+++ Wk06/day3/isSameTree.js
@@ -13,6 +13,47 @@ Input:     p =  1               q = 1
 Output: true
 */
 
-const isSameTree = function (p, q) {
-    
+const isSameTreeIterative = function (p, q) {
+    // set up either a DFS of BFS
+    // set up a stack array with roots of p & q
+    // while loop => while (stack.length)
+    // pop off p and q values off stack, and check if different return false, otherwise if (p.right && q.right)
+    // if it exits without returning false, return true
+
+    let stack = [[p, q]];
+
+    while (stack.length) {
+        let [pNode, qNode] = stack.pop();
+        if (pNode.val !== qNode.val) {
+            return false;
+        }
+
+        if (pNode.right || qNode.right) {
+            if (pNode.right && qNode.right) {
+                stack.push([pNode.right, qNode.right]);
+            } else {
+                return false
+            }
+        }
+        
+        if (pNode.left || qNode.left) {
+            if (pNode.left && qNode.left) {
+                stack.push([pNode.left, qNode.left]);
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return true;
 };
+
+const isSameTreeRecursive = function (p, q) {
+    if (!p && ! q) return true;
+
+    if (!p || !q) return false;
+
+    if (p.val !== q.val) return false;
+
+    return isSameTreeRecursive(p.left, q.left) && isSameTreeRecursive(p.right, q.right);
+}
